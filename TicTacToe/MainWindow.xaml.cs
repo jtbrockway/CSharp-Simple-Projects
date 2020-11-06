22d82c6919d8eb09ae49cda0d53e29a078b68b48
Jack Brockway <jtbrockway22@gmail.com>
2020-11-05 21:03:42 -0800

    Adding tictactoe game with bot

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+//Based on youtube tutorial by AngelSix, "C# Programming Tutorials: Beginners 09 Creating Game Tic Tac Toe with WPF", youtube link: https://www.youtube.com/watch?v=mnTyiUAHuVk
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        #region Private Members
+        /// <summary>
+        /// Reference to the Tic Tac Toe Bot
+        /// </summary>
+        private TBot bot;
+        /// <summary>
+        /// Holds current results of cells in active game
+        /// </summary>
+        private MarkType[] currVals;
+        /// <summary>
+        /// True if player 1(X) turn, or player 2(O) turn
+        /// </summary>
+        private bool player1Turn;
+        /// <summary>
+        /// True if player has toggled bot as oponent
+        /// </summary>
+        private bool botAlive;
+        /// <summary>
+        /// True if game is done, false if not
+        /// </summary>
+        private bool gameEnded;
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGame();
+        }
+        #endregion
+
+        /// <summary>
+        /// Handles resetting the field to default when a new game happens
+        /// </summary>
+        private void NewGame()
+        {
+            //Instantiate the bot
+            bot = new TBot();
+
+            //Create new array of free cells
+            currVals = new MarkType[9];
+            
+            //Explicitly set all cells to Free
+            for(int i = 0; i < 9; i++){
+                currVals[i] = MarkType.Free;
+            }
+
+            //Set to player 1's turn
+            player1Turn = true;
+
+            //Reset field to default
+            BoardContainer.Children.Cast<Button>().ToList().ForEach(btn => {
+                btn.Content = string.Empty;
+                btn.Background = Brushes.White;
+                btn.Foreground = Brushes.Blue;
+            });
+
+            gameEnded = false;
+        }
+
+        /// <summary>
+        /// Handles resetting the game 
+        /// </summary>
+        /// <param name="sender">Button that was clicked</param>
+        /// <param name="e">The click events</param>
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+
+        /// <summary>
+        /// Handles changing the content of the buttons when they are clicked
+        /// </summary>
+        /// <param name="sender">Button that was clicked</param>
+        /// <param name="e">The click events</param>
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            //If the game is completed, reset the game when a button is clicked
+            if(gameEnded)
+            {
+                NewGame();
+                return;
+            }
+
+            Button button = (Button)sender;
+
+            //Identify which button was clicked
+            int col = Grid.GetColumn(button);
+            int row = Grid.GetRow(button);
+            int index = col + (row * 3);
+            
+            if(currVals[index] != MarkType.Free)
+            {
+                return;
+            }
+
+            if(player1Turn)
+            {
+                currVals[index] = MarkType.Cross;
+                button.Content = "X";
+                button.Foreground = Brushes.Blue;
+                
+                //Bot takes turn
+                if(botAlive)
+                {
+                    int moveIndex = bot.Move(currVals); //Get the bots move
+                    currVals[moveIndex] = MarkType.Nought; //Update vals list with bots move
+                    //Update button with bot's move
+                    Button moveButton = BoardContainer.Children.Cast<Button>().ToList()[moveIndex];
+                    moveButton.Content = "O";
+                    moveButton.Foreground = Brushes.Yellow;
+
+                    player1Turn ^= true;
+                }
+            }
+            else
+            {
+                currVals[index] = MarkType.Nought;
+                button.Content = "O";
+                button.Foreground = Brushes.Red;
+            }
+            //Swap player turns
+            player1Turn ^= true;
+
+            //Check for winner
+            if(CheckWinner())
+            {
+                gameEnded = true;
+            }
+        }
+
+        /// <summary>
+        /// Handles toggling of the tic tac toe bot as the oponent
+        /// </summary>
+        /// <param name="sender">Button that was clicked</param>
+        /// <param name="e">The click events</param>
+        private void BtnBot_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (botAlive) {
+                button.Background = Brushes.White;
+                button.Content = "Bot: Off";
+                botAlive = false;
+            } 
+            else
+            {
+                button.Background = Brushes.Gray;
+                button.Content = "Bot: On";
+                botAlive = true;
+            }
+
+        }
+
+        /// <summary>
+        /// Helper method to check for a winner each time a button is pressed
+        /// </summary>
+        /// <returns>True if winner, false if not</returns>
+        private bool CheckWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int rowIndex = i * 3;
+                //Horizontal winner
+                if ((currVals[rowIndex] != MarkType.Free) && (currVals[rowIndex] == currVals[rowIndex + 1] && currVals[rowIndex] == currVals[rowIndex + 2]))
+                {
+                    ColorWinners(rowIndex, rowIndex + 1, rowIndex + 2);
+                    return true;
+                }
+                //Vertical winner
+                if ((currVals[i] != MarkType.Free) && ((currVals[i] == currVals[i + 3]) && (currVals[i] == currVals[i + 6])))
+                {
+                    ColorWinners(i, i + 3, i + 6);
+                    return true;
+                }
+                //Diagonal L-R winner
+                if (i == 0)
+                {
+                    if((currVals[0] != MarkType.Free) && ((currVals[0] == currVals[4]) && (currVals[0] == currVals[8])))
+                    {
+                        ColorWinners(0, 4, 8);
+                        return true;
+                    }
+                }
+                //Diagonal R-L winner
+                if (i == 2)
+                {
+                    if ((currVals[2] != MarkType.Free) && ((currVals[2] == currVals[4]) && (currVals[2] == currVals[6])))
+                    {
+                        ColorWinners(2, 4, 6);
+                        return true;
+                    }
+                }
+            }
+
+            //No more cells available
+            if (!currVals.Any(btn => btn == MarkType.Free))
+            {
+                gameEnded = true;
+                BoardContainer.Children.Cast<Button>().ToList().ForEach(btn => { btn.Background = Brushes.Orange; });
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Helper method used to color buttons that won
+        /// </summary>
+        private void ColorWinners(int btn1, int btn2, int btn3)
+        {
+            int counter = 0;
+            BoardContainer.Children.Cast<Button>().ToList().ForEach(btn => {
+                if(counter == btn1 || counter == btn2 || counter == btn3)
+                {
+                    btn.Background = Brushes.Green;
+                }
+                counter++;
+            });
+        }
+    }
+}
---
+++ TicTacToe/TBot.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Class to hold Tic Tac Toe bot logic. Based on Tic Tac Toe AI from Gred Surma - https://towardsdatascience.com/tic-tac-toe-creating-unbeatable-ai-with-minimax-algorithm-8af9e52c1e7d
+    /// </summary>
+    class TBot
+    {
+        private int moveHere;
+        /// <summary>
+        /// Helper function to find the winner of a current board state
+        /// </summary>
+        /// <param name="winnerBoard">The current board state to check winner against</param>
+        /// <returns>-1 if human won, 0 if tie, 1 if computer wins</returns>
+        private int FindWinner(MarkType[] winnerBoard)
+        {
+            //1 if computer, 0 if tie, -1 if human
+            int winner = 0;
+
+            //Find winner in board
+            for (int i = 0; i < 3; i++)
+            {
+                int rowIndex = i * 3;
+                //Horizontal winner
+                if ((winnerBoard[rowIndex] != MarkType.Free) && (winnerBoard[rowIndex] == winnerBoard[rowIndex + 1] && winnerBoard[rowIndex] == winnerBoard[rowIndex + 2]))
+                {
+                    //If human won, set to -1, else 1
+                    winner = winnerBoard[rowIndex] == MarkType.Cross ? -1 : 1;
+                }
+                //Vertical winner
+                if ((winnerBoard[i] != MarkType.Free) && ((winnerBoard[i] == winnerBoard[i + 3]) && (winnerBoard[i] == winnerBoard[i + 6])))
+                {
+                    winner = winnerBoard[rowIndex] == MarkType.Cross ? -1 : 1;
+                }
+                //Diagonal L-R winner
+                if (i == 0)
+                {
+                    if ((winnerBoard[0] != MarkType.Free) && ((winnerBoard[0] == winnerBoard[4]) && (winnerBoard[0] == winnerBoard[8])))
+                    {
+                        winner = winnerBoard[rowIndex] == MarkType.Cross ? -1 : 1;
+                    }
+                }
+                //Diagonal R-L winner
+                if (i == 2)
+                {
+                    if ((winnerBoard[2] != MarkType.Free) && ((winnerBoard[2] == winnerBoard[4]) && (winnerBoard[2] == winnerBoard[6])))
+                    {
+                        winner = winnerBoard[rowIndex] == MarkType.Cross ? -1 : 1;
+                    }
+                }
+            }
+            return winner;
+        }
+
+        /// <summary>
+        /// Utility function to manage creating copy of the original boards
+        /// </summary>
+        /// <param name="copyArray">Clone array to copy into</param>
+        /// <param name="originalArray">Original array to copy from</param>
+        private void MakeCopy(MarkType[] copyArray, MarkType[] originalArray)
+        {
+            for(int i = 0; i < originalArray.Length; i++)
+            {
+                copyArray[i] = originalArray[i];
+            }
+        }
+
+        /// <summary>
+        /// Algorithm that decides best move using via minimax
+        /// </summary>
+        /// <param name="board">Board state</param>
+        /// <param name="currentPlayer"> tracks the current player, -1 human, 1 if computer</param>
+        /// <returns>The score associated with the current move</returns>
+        private int DecideMove(MarkType[] decisionBoard, int currentPlayer)
+        {
+            //Check if there are any winners
+            int won = FindWinner(decisionBoard);
+            if (won != 0)
+            {
+                return won;
+            }
+
+            int score = -2; //Tracks the score
+            int moveIndex = -1; //Tracks which index will be the move
+
+            for(int i = 0; i < 9; i++)
+            {
+                //Only move on free spaces
+                if(decisionBoard[i] == MarkType.Free)
+                {
+                    MarkType[] newMoveBoard = new MarkType[decisionBoard.Length];
+                    MakeCopy(newMoveBoard, decisionBoard); // Duplicate board for testing different moves
+                    newMoveBoard[i] = currentPlayer == 1 ? MarkType.Nought : MarkType.Cross; // Fill board with new move dependent on whose turn it is
+                    int moveScore = -DecideMove(newMoveBoard, -1 * currentPlayer); // Check value of making the move
+                    if(moveScore > score)
+                    {
+                        score = moveScore;
+                        moveIndex = i;
+                    }
+                }
+            }
+            //If tie, return 0
+            if(moveIndex == -1)
+            {
+                return 0;
+            }
+
+            moveHere = moveIndex;
+            return score;
+        }
+
+        /// <summary>
+        /// Public function to handle bots move
+        /// </summary>
+        /// <param name="board">Current board state</param>
+        public int Move(MarkType[] board)
+        {
+            DecideMove(board, 1);
+            return moveHere;
+        }
+    }
+}
